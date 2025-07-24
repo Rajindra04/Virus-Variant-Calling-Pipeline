1241075e66c2bc02e0ca2d31a18a89b7a57b6d47
Rajindra04 <94608821+Rajindra04@users.noreply.github.com>
2025-07-24 22:27:50 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ setup(
     name='virus_variant_calling_pipeline',
     version='1.0.0',
     author='Rajindra Napit',
-    description='Virus variant calling pipeline for dengue',
+    description='Pipeline for dengue virus variant calling',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Rajindra04/Virus-Variant-Calling-Pipeline',
-    packages=find_packages(exclude=['conda-recipe*', 'tests*']),
-    install_requires=[
-        'pandas>=2.3.0', 'matplotlib>=3.10.3',
-        'numpy>=2.3.1', 'pyyaml>=6.0.2'
-    ],
+    packages=find_packages(exclude=['conda-recipe']),
+    install_requires=['pandas>=2.3.0','matplotlib','numpy','pyyaml'],
     entry_points={
         'console_scripts': [
             'virus-pipeline=run_pipeline:main'
@@ -20,4 +17,3 @@ setup(
     },
     python_requires='>=3.8',
 )
-
